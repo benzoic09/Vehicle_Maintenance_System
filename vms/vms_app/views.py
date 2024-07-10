0c20c14ac05b86d3bcab63cfb49094faaf387701
benzoic09 <104141553+benzoic09@users.noreply.github.com>
2024-07-10 12:43:47 +0300

    fixed landing page

--- vms/vms_app/views.py
+++ vms/vms_app/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
  
 def index(request):
   return render(request, 'index.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def services(request):
+  return render(request, 'services.html')
+
+def contact(request):
+  return render(request, 'contact.html')
